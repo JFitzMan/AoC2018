8a6f3fbd1c7f99961e435bd885739ca9c0bb5c4c
Jordan Fitzpatrick <fitzpatrickj1@email.arizona.edu>
2018-12-07 16:28:20 -0700

    day 5 pt 1

---
+++ day5/day5.js
@@ -0,0 +1,29 @@
+function checkReaction (left, right){
+    if (left == left.toUpperCase()){
+        if (right == right.toLowerCase()){
+            return left == right.toUpperCase()
+        }
+    }
+    else if (left == left.toLowerCase()){
+        if (right == right.toUpperCase()){
+            return left == right.toLowerCase()
+        }
+    }
+    return false
+}
+
+let fs = require('fs')
+let polymer = fs.readFileSync('day5/input.txt', 'utf8').split('')
+
+let i = 0
+let change = true
+while (change){
+    change = false
+    for (i=1; i<polymer.length; i++){
+        if (checkReaction(polymer[i-1], polymer[i])){
+            polymer.splice(i-1, 2)
+            change = true
+        }
+    }
+}
+console.log(polymer.length)
