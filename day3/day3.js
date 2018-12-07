4477380303e9bfdd9d6f9136cb2c9323ac988f2d
Jordan Fitzpatrick <fitzpatrickj1@email.arizona.edu>
2018-12-07 09:41:35 -0700

    day 3 pt 2

--- day3/day3.js
+++ day3/day3.js
@@ -34,3 +34,23 @@ for (i=0; i<dim; i++){
 }
 console.log(count)
 
+// pt2, loop back over array and look for the one with no overlaps
+claims.forEach( claim => {
+    num = claim.slice(0, claim.indexOf('@')).trim()
+    Xstart = parseInt(claim.slice(claim.indexOf('@')+1, claim.indexOf(',')).trim())
+    Ystart = parseInt(claim.slice(claim.indexOf(',')+1, claim.indexOf(':')).trim())
+    width = parseInt(claim.slice(claim.indexOf(':')+1, claim.indexOf('x')).trim())
+    height = parseInt(claim.slice(claim.indexOf('x')+1).trim())
+
+    let row = Ystart
+    let flag = true
+    for (row; row < height+Ystart; row++){
+        let col = Xstart
+        for (col; col < width+Xstart; col++){
+            if (fabric[row][col] > 1){
+                flag = false
+            }
+        }
+    }
+    if (flag == true) {console.log(num)}
+})
